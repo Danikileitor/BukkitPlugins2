cd03d23b579457eaba77f26e268557ef295dbeb8
Danikileitor <focusyi@hotmail.com>
2022-10-02 16:09:44 +0200

    muchos más comandos

--- testeos/src/main/java/dnk/testeos/Main.java
+++ testeos/src/main/java/dnk/testeos/Main.java
@@ -38,6 +38,21 @@ public class Main extends JavaPlugin implements Listener {
     }
   }
 
+  @EventHandler
+  public void onPlayerJoin(PlayerJoinEvent e) {
+    Player p = e.getPlayer();
+    World w = p.getWorld();
+    LOGGER.info(p.getDisplayName() + " ha entrado al server.");
+    p.sendTitle("Bienvenido al server", p.getDisplayName(), 50, 300, 50);
+    w.playSound(p.getLocation(), Sound.ITEM_GOAT_HORN_SOUND_2, 100, 1);
+    Bukkit.getScheduler().runTaskLater(this, () -> {
+      w.playSound(p.getLocation(), Sound.ITEM_GOAT_HORN_SOUND_1, 100, 1);
+    }, 200L);
+    Bukkit.getScheduler().runTaskLater(this, () -> {
+      w.playSound(p.getLocation(), Sound.ITEM_GOAT_HORN_SOUND_5, 100, 1);
+    }, 250L);
+  }
+
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     Player p = (Player) sender;
@@ -46,11 +61,13 @@ public class Main extends JavaPlugin implements Listener {
         if (p.getHealth() > 1) {
           p.chat("Me muero");
           p.damage(1);
+          return true;
         } else {
           p.chat("Casi me muero");
           p.damage(0);
           p.playEffect(EntityEffect.TOTEM_RESURRECT);
           p.setHealth(p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue());
+          return true;
         }
       } else {
         sender.sendMessage("§4You don't have permission to use this command.");
@@ -100,6 +117,7 @@ public class Main extends JavaPlugin implements Listener {
           Player o = Bukkit.getPlayer(args[0]);
           o.setAbsorptionAmount(Double.parseDouble(args[1]));
           sender.sendMessage(o.getDisplayName() + " obtiene vida extra: " + o.getAbsorptionAmount());
+          return true;
         } else {
           sender.sendMessage("§4Has introducido mal los parámetros del comando.");
         }
@@ -128,6 +146,7 @@ public class Main extends JavaPlugin implements Listener {
           o.setWalkSpeed(Float.parseFloat(args[1]) / 10.0f);
           float s = o.getWalkSpeed() * 10.0f;
           sender.sendMessage("Velocidad de " + o.getDisplayName() + " modificada: " + s);
+          return true;
         } else {
           sender.sendMessage("§4Has introducido mal los parámetros del comando.");
         }
@@ -154,6 +173,7 @@ public class Main extends JavaPlugin implements Listener {
           Player o = Bukkit.getPlayer(args[0]);
           o.setFlySpeed(Float.parseFloat(args[1]));
           sender.sendMessage("Velocidad de vuelo de " + o.getDisplayName() + " modificada: " + p.getFlySpeed());
+          return true;
         } else {
           sender.sendMessage("§4Has introducido mal los parámetros del comando.");
         }
@@ -191,36 +211,42 @@ public class Main extends JavaPlugin implements Listener {
         sender.sendMessage("§4You don't have permission to use this command.");
       }
       return true;
-    }
-    if (command.getName().equalsIgnoreCase("knb")) {
-      if (sender.hasPermission("test.knb")) {
-        if (args.length == 0) {
-          p.getAttribute(Attribute.GENERIC_ATTACK_KNOCKBACK).setBaseValue(1.0);
-          sender.sendMessage(
-              "Daño de retroceso por defecto: " + p.getAttribute(Attribute.GENERIC_ATTACK_KNOCKBACK).getValue());
-          return true;
-        }
-        if (args.length == 1) {
-          p.getAttribute(Attribute.GENERIC_ATTACK_KNOCKBACK).setBaseValue(Double.parseDouble(args[0]));
-          sender.sendMessage(
-              "Daño de retroceso modificado: " + p.getAttribute(Attribute.GENERIC_ATTACK_KNOCKBACK).getValue());
-          return true;
-        }
-        if (args.length == 2 && Bukkit.getPlayer(args[0]) != null) {
-          Player o = Bukkit.getPlayer(args[0]);
-          o.getAttribute(Attribute.GENERIC_ATTACK_KNOCKBACK).setBaseValue(Double.parseDouble(args[1]));
-          sender.sendMessage("El daño de retroceso de " + o.getDisplayName() + " ha sido modificado: "
-              + p.getAttribute(Attribute.GENERIC_ATTACK_KNOCKBACK).getValue());
-          return true;
-        } else {
-          sender.sendMessage("§4Has introducido mal los parámetros del comando.");
-        }
-        return true;
-      } else {
-        sender.sendMessage("§4You don't have permission to use this command.");
-      }
-      return true;
-    }
+    } /*
+       * if (command.getName().equalsIgnoreCase("knb")) {
+       * if (sender.hasPermission("test.knb")) {
+       * if (args.length == 0) {
+       * p.getAttribute(Attribute.GENERIC_ATTACK_KNOCKBACK).setBaseValue(1.0);
+       * sender.sendMessage(
+       * "Daño de retroceso por defecto: " +
+       * p.getAttribute(Attribute.GENERIC_ATTACK_KNOCKBACK).getValue());
+       * return true;
+       * }
+       * if (args.length == 1) {
+       * p.getAttribute(Attribute.GENERIC_ATTACK_KNOCKBACK).setBaseValue(Double.
+       * parseDouble(args[0]));
+       * sender.sendMessage(
+       * "Daño de retroceso modificado: " +
+       * p.getAttribute(Attribute.GENERIC_ATTACK_KNOCKBACK).getValue());
+       * return true;
+       * }
+       * if (args.length == 2 && Bukkit.getPlayer(args[0]) != null) {
+       * Player o = Bukkit.getPlayer(args[0]);
+       * o.getAttribute(Attribute.GENERIC_ATTACK_KNOCKBACK).setBaseValue(Double.
+       * parseDouble(args[1]));
+       * sender.sendMessage("El daño de retroceso de " + o.getDisplayName() +
+       * " ha sido modificado: "
+       * + p.getAttribute(Attribute.GENERIC_ATTACK_KNOCKBACK).getValue());
+       * return true;
+       * } else {
+       * sender.sendMessage("§4Has introducido mal los parámetros del comando.");
+       * }
+       * return true;
+       * } else {
+       * sender.sendMessage("§4You don't have permission to use this command.");
+       * }
+       * return true;
+       * }
+       */
     if (command.getName().equalsIgnoreCase("spa")) {
       if (sender.hasPermission("test.spa")) {
         if (args.length == 0) {
@@ -250,21 +276,154 @@ public class Main extends JavaPlugin implements Listener {
       }
       return true;
     }
+    if (command.getName().equalsIgnoreCase("fly")) {
+      if (sender.hasPermission("test.fly")) {
+        if (args.length == 0 && p.getAllowFlight() == true) {
+          p.setAllowFlight(false);
+          sender.sendMessage("Vuelo desactivado");
+          return true;
+        }
+        if (args.length == 0 && p.getAllowFlight() == false) {
+          p.setAllowFlight(true);
+          sender.sendMessage("Vuelo activado");
+          return true;
+        }
+        if (args.length == 1 && args[0].equals("1") || args.length == 1 && args[0].equals("true")) {
+          p.setAllowFlight(true);
+          sender.sendMessage("Vuelo activado");
+          return true;
+        }
+        if (args.length == 1 && args[0].equals("0") || args.length == 1 && args[0].equals("false")) {
+          p.setAllowFlight(false);
+          sender.sendMessage("Vuelo desactivado");
+          return true;
+        }
+        if (args.length == 2 && Bukkit.getPlayer(args[0]) != null) {
+          Player o = Bukkit.getPlayer(args[0]);
+          if (args[1].equals("1") || args[1].equals("true")) {
+            o.setAllowFlight(true);
+            sender.sendMessage("Vuelo activado para " + o.getDisplayName());
+            return true;
+          }
+          if (args[1].equals("0") || args[1].equals("false")) {
+            o.setAllowFlight(false);
+            sender.sendMessage("Vuelo desactivado para " + o.getDisplayName());
+            return true;
+          }
+        } else {
+          sender.sendMessage("§4Has introducido mal los parámetros del comando.");
+        }
+        return true;
+      } else {
+        sender.sendMessage("§4You don't have permission to use this command.");
+      }
+      return true;
+    }
+    if (command.getName().equalsIgnoreCase("hide")) {
+      if (sender.hasPermission("test.hide")) {
+        if (args.length == 0) {
+          for (Player ps : Bukkit.getOnlinePlayers()) {
+            ps.hidePlayer(this, p);
+          }
+          sender.sendMessage("Invisibilidad activada");
+          return true;
+        }
+        if (args.length == 1 && Bukkit.getPlayer(args[0]) != null) {
+          Player o = Bukkit.getPlayer(args[0]);
+          for (Player ps : Bukkit.getOnlinePlayers()) {
+            ps.hidePlayer(this, o);
+          }
+          sender.sendMessage("Invisibilidad activada para " + o.getDisplayName());
+          return true;
+        } else {
+          sender.sendMessage("§4Has introducido mal los parámetros del comando.");
+        }
+        return true;
+      } else {
+        sender.sendMessage("§4You don't have permission to use this command.");
+      }
+      return true;
+    }
+    if (command.getName().equalsIgnoreCase("unhide")) {
+      if (sender.hasPermission("test.unhide")) {
+        if (args.length == 0) {
+          for (Player ps : Bukkit.getOnlinePlayers()) {
+            ps.showPlayer(this, p);
+          }
+          sender.sendMessage("Invisibilidad desactivada");
+          return true;
+        }
+        if (args.length == 1 && Bukkit.getPlayer(args[0]) != null) {
+          Player o = Bukkit.getPlayer(args[0]);
+          for (Player ps : Bukkit.getOnlinePlayers()) {
+            ps.showPlayer(this, o);
+          }
+          sender.sendMessage("Invisibilidad desactivada para " + o.getDisplayName());
+          return true;
+        } else {
+          sender.sendMessage("§4Has introducido mal los parámetros del comando.");
+        }
+        return true;
+      } else {
+        sender.sendMessage("§4You don't have permission to use this command.");
+      }
+      return true;
+    }
+    if (command.getName().equalsIgnoreCase("nick")) {
+      if (sender.hasPermission("test.nick")) {
+        if (args.length == 0) {
+          p.setDisplayName(p.getName());
+          p.setPlayerListName(p.getName());
+          sender.sendMessage("Nick cambiado a " + p.getDisplayName());
+          return true;
+        }
+        if (args.length == 1 && Bukkit.getPlayerExact(args[0]) == null) {
+          p.setDisplayName(args[0]);
+          p.setPlayerListName(args[0]);
+          sender.sendMessage("Nick cambiado a " + p.getDisplayName());
+          return true;
+        }
+        if (args.length == 2 && Bukkit.getPlayer(args[0]) != null && Bukkit.getPlayerExact(args[1]) != null) {
+          Player o = Bukkit.getPlayer(args[0]);
+          o.setDisplayName(args[1]);
+          o.setPlayerListName(args[1]);
+          sender.sendMessage("Nick de " + o.getName() + " cambiado a " + o.getDisplayName());
+          return true;
+        } else {
+          sender.sendMessage("§4Has introducido mal los parámetros del comando.");
+        }
+        return true;
+      } else {
+        sender.sendMessage("§4You don't have permission to use this command.");
+      }
+      return true;
+    }
+    if (command.getName().equalsIgnoreCase("aire")) {
+      if (sender.hasPermission("test.aire")) {
+        if (args.length == 0) {
+          p.setMaximumAir(300);
+          sender.sendMessage("Aguante de respiración modificado a " + p.getMaximumAir() / 20 + " segundos.");
+          return true;
+        }
+        if (args.length == 1) {
+          p.setMaximumAir(Integer.parseInt(args[0]) * 20);
+          sender.sendMessage("Aguante de respiración modificado a " + p.getMaximumAir() / 20 + " segundos.");
+          return true;
+        }
+        if (args.length == 2 && Bukkit.getPlayer(args[0]) != null) {
+          Player o = Bukkit.getPlayer(args[0]);
+          o.setMaximumAir(Integer.parseInt(args[1]) * 20);
+          sender.sendMessage("Aguante de respiración modificado a " + o.getMaximumAir() / 20 + " segundos.");
+          return true;
+        } else {
+          sender.sendMessage("§4Has introducido mal los parámetros del comando.");
+        }
+        return true;
+      } else {
+        sender.sendMessage("§4You don't have permission to use this command.");
+      }
+      return true;
+    }
     return false;
-  }
-
-  @EventHandler
-  public void onPlayerJoin(PlayerJoinEvent e) {
-    Player p = e.getPlayer();
-    World w = p.getWorld();
-    LOGGER.info(p.getDisplayName() + " ha entrado al server.");
-    p.sendTitle("Bienvenido al server", p.getDisplayName(), 50, 300, 50);
-    w.playSound(p.getLocation(), Sound.ITEM_GOAT_HORN_SOUND_2, 100, 1);
-    Bukkit.getScheduler().runTaskLater(this, () -> {
-      w.playSound(p.getLocation(), Sound.ITEM_GOAT_HORN_SOUND_1, 100, 1);
-    }, 200L);
-    Bukkit.getScheduler().runTaskLater(this, () -> {
-      w.playSound(p.getLocation(), Sound.ITEM_GOAT_HORN_SOUND_5, 100, 1);
-    }, 250L);
   }
 }
